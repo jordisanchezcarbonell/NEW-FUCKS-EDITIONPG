68b490b88a3be172f12d71945e5e3c216160f4fd
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2021-02-06 15:46:41 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost:3000",
+        "BASE_URL": "https://new-fucks-editionpg-6kqgg3mg0.vercel.app",
         "MONGODB_URL":"mongodb+srv://kazukun:Kazukun23@cluster0.u7dsw.mongodb.net/nextjs?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET":"u5@eXE7rbTwDppVkj%PgpAUp_!4pU-c-B!hgqg#$Q5YpJvKcUL",
         "REFRESH_TOKEN_SECRET":"?vwdw$&w5S^rVkVV$4qJ2t&gmYQ^Y@*r9b@PactVn8?BDVqqj?$ptwC7CKGgtJ3pK=u?6=dy427bU-=&",
